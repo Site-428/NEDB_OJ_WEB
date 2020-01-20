c7995ac72dbc5b880d841866dbdeb427e11d4bb6
Administrator <admin@example.com>
2020-01-20 11:02:29 +0800

    更新 search.php

--- search.php
+++ search.php
@@ -76,7 +76,7 @@
 	$TAG=["EXAM","CLA","CODE","STAT","GRD","MSC","WSC","SEX","LOC","KNO","SPJ"];
 	$GRADE1=["大一","本科一年级","大学一年级","FRESHMAN","一年级","一"];
 	$SPC=["重修","困难"];
-	$db=mysqli_connect('localhost','root','密码','NEDB');
+	$db=mysqli_connect('localhost','TCDCPP','TCDCPP','TCD428');
 	$db->query("set names utf8");
 	if(!$db)
 	{
